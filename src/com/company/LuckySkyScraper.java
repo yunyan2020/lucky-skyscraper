0e6a53d1e21f6379900fc26e0578076aa95b0463
yunyan2020 <li_yunyan@hotmail.com>
2020-09-30 09:38:45 +0200

    Intellij autumatisk update

--- src/com/company/LuckySkyScraper.java
+++ src/com/company/LuckySkyScraper.java
@@ -1,5 +1,42 @@
 package com.company;
 
 public class LuckySkyScraper {
+    static private boolean showTiming = true;
+
+    static public int toRealFloor(int fakeFloorGoal){
+        var startTime = System.nanoTime();
+        var fakeFloor = 1;
+        for(var realFloor = 1; realFloor <= fakeFloorGoal; realFloor++){
+            if(fakeFloorGoal == fakeFloor) {
+                var endTime = System.nanoTime();
+                var timeTaken = endTime - startTime;
+                if(showTiming){
+                    System.out.println("toRealFloor took (ns): " + timeTaken);
+                }
+                return realFloor; // this ends the loop and the method
+            }
+            do {
+                fakeFloor++;
+            } while(fakeFloor % 10 == 4 || fakeFloor % 100 == 13);
+        }
+        return -1; // could not find real floor number
+    }
+
+    static public int toFakeFloor(int realFloorGoal){
+        var startTime = System.nanoTime();
+        var fakeFloor = 1;
+        for(var realFloor = 1; realFloor < realFloorGoal; realFloor++){
+            do {
+                fakeFloor++;
+            } while(fakeFloor % 10 == 4 || fakeFloor % 100 == 13);
+        }
+        var endTime = System.nanoTime();
+        var timeTaken = endTime - startTime;
+        if(showTiming) {
+            System.out.println("toFakeFloor took (ns): " + timeTaken);
+        }
+        return fakeFloor;
+    }
+
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	  for(var floor = 1; floor <= 220; floor++){
+            System.out.print("floor: " + floor);
+            var fakeFloor = LuckySkyScraper.toFakeFloor(floor);
+            System.out.print("  fake floor: " + fakeFloor);
+            var backToRealFloor = LuckySkyScraper.toRealFloor(fakeFloor);
+            System.out.println("  back to real: " + backToRealFloor);
+        }
     }
 }
